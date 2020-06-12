d48e16cee391d163baac5ea3e4929c96f7e45bf3
keet <wwukuww@gmail.com>
2020-06-12 11:21:35 +0900

    made code

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -1,5 +1,6 @@
 package Main;
-
+import Calculation.*;
+import java.util.Scanner;
 /**
  * 여기에 MyApp 클래스 설명을 작성하십시오.
  * 
@@ -9,5 +10,13 @@ package Main;
 public class MyApp
 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        boolean isOldMovie = true;
+        System.out.println("구작인 가요?");
+        int age = 0;
+        System.out.print("나이를 입력하세요 : ");
+        age = sc.nextInt();
+        RentalDVDService R = new RentalDVDService();
+        R.discountCalculation(age, isOldMovie);
     }
 } 
